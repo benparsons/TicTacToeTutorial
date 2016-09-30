433816c086510985f87120ecf665add4e958407d
Ben Parsons <ben@bpulse.co.uk>
2016-09-30 15:40:26 +0100

    end of section 3, basic GridView with ArrayAdapter

--- app/src/main/java/org/bpulse/tictactoetutorial/MainActivity.java
+++ app/src/main/java/org/bpulse/tictactoetutorial/MainActivity.java
@@ -2,12 +2,26 @@ package org.bpulse.tictactoetutorial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+  public final String[] boardArray = new String[9];
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    final ArrayAdapter<String> arrayAdapter =
+            new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, boardArray);
+    final GridView board = (GridView)findViewById(R.id.gvBoard);
+
+    board.setAdapter(arrayAdapter);
+
+    for (int i = 0; i < boardArray.length; i++) {
+      boardArray[i] = "X";
+    }
   }
 }
